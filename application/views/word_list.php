7c2fa81ef0af7738f9a66ee51c37d0e5604ca051
yhaay <yyhaa123@gmail.com>
2015-03-08 17:27:38 +0900

    aefww

--- application/views/word_list.php
+++ application/views/word_list.php
@@ -1,6 +1,6 @@
 <ul class="list-group">
 	<?php foreach($word_list as $list):?>
-	<a href="/word/detail/<?=$word->wordidx?>" class="list-group-item list-group-item-info"><?= $list->word?></a>
+	<a href="/word/detail/<?=$list->wordidx?>" class="list-group-item list-group-item-info"><?= $list->word?></a>
 	<?php if (!is_null($list->meaning)):?>
 	<li class="list-group-item">
 		<?= $list->meaning?>
