459045142b43abbb2c77e16ed317d27018ccb1d7
yhaay <yyhaa123@gmail.com>
2015-03-09 00:11:46 +0900

    aefaw

--- application/views/meaning_list.php
+++ application/views/meaning_list.php
@@ -4,22 +4,25 @@
 	<?php foreach($meaning_list as $list):?>
 	<li class="list-group-item">
 		<?= $list->meaning?>
-		<span meaningidx="<?=$list->meaningidx?>" class="badge referral"><?= $list->referral?></span>
+		<a href=""><span meaningidx="<?=$list->meaningidx?>"
+			class="badge referral"><?= $list->referral?></span></a>
 	</li>
 	<?php endforeach; ?>
 </ul>
 
-<div class="alert alert-success">
-	추천되었습니다.
+<div id="alert_refer" class="alert alert-success">
+	<a href="#" class="close" data-dismiss="alert">&times;</a> 추천되었습니다.
 </div>
 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript"
+	src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript">
+	$('.alert').hide();
 
 		$('.referral').click(function() {
+			var button = $(this);
 			var referral = parseInt($(this).html());
 			var meaningidx = $(this).attr('meaningidx');
-			$(this).html(referral+1);
 			var post_data = {
 					'meaningidx' : meaningidx,
 					'<?= $this->security->get_csrf_token_name() ?>': '<?=$this->security->get_csrf_hash()?>'
@@ -30,7 +33,8 @@
 				url: "/word/update_referral",
 				data: post_data,
 				success: function() {
-					$('.alert').alert();
+					button.html(referral+1);
+					$('#alert_refer').show();
 				}
 			});
 		});
