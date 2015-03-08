189a54fcc37d6f4a8b20986b135019335a1c6bd8
yhaay <yyhaa123@gmail.com>
2015-03-08 15:55:40 +0900

    awefaw

--- application/views/choseong_list.php
+++ application/views/choseong_list.php
@@ -1,16 +1,16 @@
 <ul class="pagination">
-  <li><a href="/word/choseong/1">ㄱ</a></li>
-  <li><a href="/word/choseong/2">ㄴ</a></li>
-  <li><a href="/word/choseong/3">ㄷ</a></li>
-  <li><a href="/word/choseong/4">ㄹ</a></li>
-  <li><a href="/word/choseong/5">ㅁ</a></li>
-  <li><a href="/word/choseong/6">ㅂ</a></li>
-  <li><a href="/word/choseong/7">ㅅ</a></li>
-  <li><a href="/word/choseong/8">ㅇ</a></li>
-  <li><a href="/word/choseong/9">ㅈ</a></li>
-  <li><a href="/word/choseong/10">ㅊ</a></li>
-  <li><a href="/word/choseong/11">ㅋ</a></li>
-  <li><a href="/word/choseong/12">ㅌ</a></li>
-  <li><a href="/word/choseong/13">ㅍ</a></li>
-  <li><a href="/word/choseong/14">ㅎ</a></li>
+  <li><a <?php if ($choseong == '1'): ?>class="active"<?php endif; ?> href="/word/choseong/1">ㄱ</a></li>
+  <li><a <?php if ($choseong == '2'): ?>class="active"<?php endif; ?> href="/word/choseong/2">ㄴ</a></li>
+  <li><a <?php if ($choseong == '3'): ?>class="active"<?php endif; ?> href="/word/choseong/3">ㄷ</a></li>
+  <li><a <?php if ($choseong == '4'): ?>class="active"<?php endif; ?> href="/word/choseong/4">ㄹ</a></li>
+  <li><a <?php if ($choseong == '5'): ?>class="active"<?php endif; ?> href="/word/choseong/5">ㅁ</a></li>
+  <li><a <?php if ($choseong == '6'): ?>class="active"<?php endif; ?> href="/word/choseong/6">ㅂ</a></li>
+  <li><a <?php if ($choseong == '7'): ?>class="active"<?php endif; ?> href="/word/choseong/7">ㅅ</a></li>
+  <li><a <?php if ($choseong == '8'): ?>class="active"<?php endif; ?> href="/word/choseong/8">ㅇ</a></li>
+  <li><a <?php if ($choseong == '9'): ?>class="active"<?php endif; ?> href="/word/choseong/9">ㅈ</a></li>
+  <li><a <?php if ($choseong == '10'): ?>class="active"<?php endif; ?> href="/word/choseong/10">ㅊ</a></li>
+  <li><a <?php if ($choseong == '11'): ?>class="active"<?php endif; ?> href="/word/choseong/11">ㅋ</a></li>
+  <li><a <?php if ($choseong == '12'): ?>class="active"<?php endif; ?> href="/word/choseong/12">ㅌ</a></li>
+  <li><a <?php if ($choseong == '13'): ?>class="active"<?php endif; ?> href="/word/choseong/13">ㅍ</a></li>
+  <li><a <?php if ($choseong == '14''): ?>class="active"<?php endif; ?> href="/word/choseong/14">ㅎ</a></li>
 </ul>
