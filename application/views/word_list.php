bf138b31a4507cbf71bd506517965ee0c6e46472
yhaay <yyhaa123@gmail.com>
2015-03-08 16:58:48 +0900

    afwef

--- application/views/word_list.php
+++ application/views/word_list.php
@@ -1,8 +1,10 @@
-<ul>
+<ul class="list-group">
 	<?php foreach($word_list as $list):?>
-	<li><?= $list->word?></li>
-	<li><?= $list->meaning?>
-	<?= $list->referral?></li>
+	<a href="#" class="list-group-item"><?= $list->word?></a>
+	<li class="list-group-item list-group-item-info">
+		<?= $list->meaning?>
+		<span class="badge"><?= $list->referral?></span>
+	</li>
 	<input type="hidden" value="<?= $list->wordidx?>">
 	<?php endforeach; ?>
 </ul>
