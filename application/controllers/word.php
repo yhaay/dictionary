0ac1f343d31c8869c14edc6c7a8e781d565b6094
yhaay <yyhaa123@gmail.com>
2015-03-10 01:15:46 +0900

    asdfsadf

--- application/controllers/word.php
+++ application/controllers/word.php
@@ -65,14 +65,14 @@ class Word extends CI_Controller {
 		$this->load->view ( 'footer' );
 	}
 	public function insert_meaning() {
-		$wordidx = $this->input->post('wordidx');
 		$this->load->model('word_model');
 		$this->word_model->insert_meaning(array(
 				'meaning'=>$this->input->post('meaning'),
 				'wordidx'=>$this->input->post('wordidx')
 		));
+		$url = '/word/detail/' + $this->input->post('wordidx');
 		$this->load->helper('url');
-		redirect('/word/detail/' + $wordidx);
+		redirect($url);
 	}
 }
 
